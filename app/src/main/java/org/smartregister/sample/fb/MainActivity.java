84e1a544a3c36929c2d4ff4c684010b438b136a3
zzainulabidin <zzainulabidin>
2020-03-30 21:20:48 +0500

    user Interface

---
+++ app/src/main/java/org/smartregister/sample/fb/MainActivity.java
@@ -0,0 +1,14 @@
+package org.smartregister.sample.fb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ familyplanningmodule/src/main/java/org/smartregister/fp/login/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package org.smartregister.fp.login.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.smartregister.fp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
