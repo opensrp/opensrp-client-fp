86deffe03afbc04e57fce17c1623bc8a41ecc076
zainulabidin <zain.ulabidin@venturedive.com>
2020-03-31 13:07:30 +0500

    user Interface

--- app/src/androidTest/java/org/smartregister/sample/fp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/smartregister/sample/fp/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("org.smartregister.sample.fb", appContext.getPackageName());
+        assertEquals("org.smartregister.sample.fp", appContext.getPackageName());
     }
 }
--- familyplanningmodule/src/main/java/org/smartregister/fp/login/ui/LoginActivity.java
+++ familyplanningmodule/src/main/java/org/smartregister/fp/login/ui/LoginActivity.java
@@ -5,12 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.smartregister.fp.R;
+import org.smartregister.view.activity.BaseLoginActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseLoginActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected int getContentView() {
+        return 0;
+    }
+
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    public void goToHome(boolean b) {
+
     }
 }
