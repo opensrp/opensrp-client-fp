21fe44d83bc3ed16f15c95d04e0115c71298ca79
syedowaisali90 <62104757+syedowaisali90@users.noreply.github.com>
2020-05-17 14:51:26 +0500

    get updated client info from db

--- opensrp-fp/src/main/java/org/smartregister/fp/common/util/Utils.java
+++ opensrp-fp/src/main/java/org/smartregister/fp/common/util/Utils.java
@@ -54,6 +54,7 @@ import org.smartregister.fp.common.model.PreviousContact;
 import org.smartregister.fp.common.model.Task;
 import org.smartregister.fp.common.rule.FPAlertRule;
 import org.smartregister.fp.features.home.repository.ContactTasksRepository;
+import org.smartregister.fp.features.home.repository.PatientRepository;
 import org.smartregister.fp.features.home.schedules.SchedulesEnum;
 import org.smartregister.fp.features.home.schedules.model.ScheduleModel;
 import org.smartregister.fp.features.home.view.HomeRegisterActivity;
@@ -252,6 +253,8 @@ public class Utils extends org.smartregister.util.Utils {
      */
     public static void proceedToContact(String baseEntityId, HashMap<String, String> personObjectClient, Context context) {
         try {
+
+            personObjectClient = PatientRepository.getClientProfileDetails(baseEntityId);
 
             String nextContact = personObjectClient.get(DBConstantsUtils.KeyUtils.NEXT_CONTACT);
             personObjectClient.put(DBConstantsUtils.KeyUtils.NEXT_CONTACT, nextContact == null ? "1" : nextContact);
